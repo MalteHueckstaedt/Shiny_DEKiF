a71aa11bce815786afec4ef3ebb0e8004938f612
Malte Hückstädt <hueckstaedt@dzhw.eu>
2022-03-10 16:57:39 +0100

    Update ggplot_density.R

--- helper_files/ggplot_density.R
+++ helper_files/ggplot_density.R
@@ -10,6 +10,12 @@ dt <- read_excel("/Volumes/USB_MALTE/df_dekif.xlsx",
                        na = c("-9995","-9990","-9991","-9992","-999", "-99")) %>% select(problm_1,problm_2) %>% na.omit() %>%
   mutate(commitment=problm_1+problm_2)
 
+saveRDS(dt,"/Users/maltehueckstaedt/Documents/Dokumente/GitHub/Shiny_DEKiF/data/problems.rds")
+
 ggplot(dt, aes(x=commitment)) + 
   #geom_histogram(aes(y=..density..), colour="black", fill="white")+
-  geom_density(alpha=.2, fill="#FF6666", bw=0.5) 
+  geom_density(alpha=.2, fill="#FF6666", bw=0.5)+
+  geom_vline(xintercept = 5, color="red")+ 
+  annotate("text", x = 5, y = 0.1, label = "Your Score", 
+           vjust = 1.5,angle = 90, color="red")
+ 
